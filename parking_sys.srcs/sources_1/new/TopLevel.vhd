0665dad3ef94e198543908f22a4ad42b1c1a2141
janobsivac <159461221+janobsivac@users.noreply.github.com>
2024-05-02 00:39:01 +0200

    Update TopLevel.vhd

--- parking_sys.srcs/sources_1/new/TopLevel.vhd
+++ parking_sys.srcs/sources_1/new/TopLevel.vhd
@@ -1,35 +1,35 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity TopLevel is
+entity TopLevel is                             --definovaní portu v top_level
     Port (
-        CLK100MHZ    : in  STD_LOGIC;
-        BTNC         : in  STD_LOGIC; 
-        echo_in      : in  STD_LOGIC;
-        trigger_out  : out STD_LOGIC;
-        CA           : out std_logic; 
+        CLK100MHZ    : in  STD_LOGIC; --hodiny
+        BTNC         : in  STD_LOGIC; --rst tlacitko
+        echo_in      : in  STD_LOGIC; --vstupní signál ze senzoru
+        trigger_out  : out STD_LOGIC; --výstupní signál do senzoru
+        CA           : out std_logic; --katody pro 7segmentovy displej 
         CB           : out std_logic; 
         CC           : out std_logic; 
         CD           : out std_logic; 
         CE           : out std_logic; 
         CF           : out std_logic; 
         CG           : out std_logic;  
-        AN : out STD_LOGIC_VECTOR (7 downto 0);
+        AN : out STD_LOGIC_VECTOR (7 downto 0); --anody
         DP : out STD_LOGIC;
-        LED : out std_logic_vector (8 downto 0));
+        LED : out std_logic_vector (8 downto 0)); --ledky pro vzdalenost
         
 end TopLevel;
 
 architecture Behavioral of TopLevel is
 
-    component clk_gen is
+    component clk_gen is --component ke cloclConverter
     port(   clk : in std_logic;
             clk_mod : out std_logic;
             divide_value : in integer:= 2000
             );
     end component;
 
-    component Trigger is
+    component Trigger is --component k Triggeru
     generic (
     N_PERIODS : integer
     );
@@ -41,7 +41,7 @@ architecture Behavioral of TopLevel is
         );
     end component;
 
-    component DistanceCalculation is
+    component DistanceCalculation is --component k DistanceCalculation
         Port (
             echo     : in  STD_LOGIC;
             distance : out STD_LOGIC_VECTOR (7 downto 0);
@@ -50,7 +50,7 @@ architecture Behavioral of TopLevel is
         );
     end component;
 
-    component Driver is
+    component Driver is --component k Driveru
         Port (
             distance : in  STD_LOGIC_VECTOR (7 downto 0);
             segment_data : out STD_LOGIC_VECTOR (6 downto 0);
@@ -59,18 +59,18 @@ architecture Behavioral of TopLevel is
         );
     end component;
 
-    signal distance_signal : std_logic_vector(7 downto 0);
+    signal distance_signal : std_logic_vector(7 downto 0); --vytvorene signály pro propojeni componentu
     signal sig_running : std_logic;
     signal clk_mod_signal : std_logic;
 
 begin
 
-    clock_instance : clk_gen
+    clock_instance : clk_gen    --napojeni vstupnich_vystupnich portu 
     port map(
     clk => CLK100MHZ,
     clk_mod => clk_mod_signal);
 
-    trigger_instance: Trigger 
+    trigger_instance: Trigger     --napojeni vstupnich_vystupnich portu 
     generic map (
             N_PERIODS => 2
         )
@@ -81,17 +81,17 @@ begin
         en_in => sig_running 
     );
 
-    distance_instance: DistanceCalculation port map (
+    distance_instance: DistanceCalculation port map (    --napojeni vstupnich_vystupnich portu 
         echo => echo_in,
         distance => distance_signal,
         en_out => sig_running,
         clk => clk_mod_signal
     );
 
-    driver_instance: Driver port map (
+    driver_instance: Driver port map (    --napojeni vstupnich_vystupnich portu 
         distance => distance_signal,
         rst => BTNC,
-        segment_data(6) => CA,
+        segment_data(6) => CA, --napojeni segment_data na katody
         segment_data(5) => CB,
         segment_data(4) => CC,
         segment_data(3) => CD,
@@ -104,6 +104,7 @@ begin
 
  
     
-    AN <= b"1111_1110";
+    AN <= b"1111_1110"; --urceni rozsviceni 7segmentoveho dispeje
     DP <= '1';
 end Behavioral;
+
